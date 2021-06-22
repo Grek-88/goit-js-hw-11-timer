a77963527fd21ec9d4efcbf8ebe3bc3eb1df2c89
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-06-22 12:01:05 +0300

    доделал интерфейс

--- src/index.js
+++ src/index.js
@@ -1,66 +1,67 @@
-// class CountdownTimer { 
-//     constructor() { }
-// };
-
-// const timer1 = new CountdownTimer({
-    //     selector: '#timer-1',
-    //     targetDate: new Date('Jun 28, 2021'),
-    // });
-    
-    const targetDate = new Date('Jun 28, 2021');
-    
-    
-    
-    
-    // setInterval(() => {
-    //     const timeNow = Date.now();
-    //     const time = targetDate - timeNow;
-    //     const counter = getTimeComponents(time);
-    // console.log(`${counter.days}:${counter.hours}:${counter.mins}:${counter.secs}`);
-    // }, 1000)
-    
-
-
-// console.log(time);
-
-function getTimeComponents(time) {
-/*
-* Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-* миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-*/
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-// console.log(days);
-
-/*
-* Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-* остатка % и делим его на количество миллисекунд в одном часе
-* (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-*/
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// console.log(hours);
-
-/*
-* Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-* миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-*/
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-// console.log(mins);
-
-/*
-* Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-* миллисекунд в одной секунде (1000)
-*/
-const secs = Math.floor((time % (1000 * 60)) / 1000);
-// console.log(secs);
-    
-return { days, hours, mins, secs }
+const refs = {
+    days: document.querySelector('span[data-value="days"]'),
+    hours: document.querySelector('span[data-value="hours"]'),
+    mins: document.querySelector('span[data-value="mins"]'),
+    secs: document.querySelector('span[data-value="secs"]')
 };
 
 
-// console.log(days1);
+class CountdownTimer { 
+    constructor({targetDate, onTick}) {
+        this.targetDate = targetDate;
+        this.onTick = onTick;
+        this.start();
+    }
+    
+    start() {
+         setInterval(() => {
+            const timeNow = Date.now();
+            const deltaTime = this.targetDate - timeNow;
+            const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
+            this.onTick({ days, hours, mins, secs })
+        }, 1000);
+    }
 
+    getTimeComponents(time) {
+    /*
+    * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+    * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+    */
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    /*
+    * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+    * остатка % и делим его на количество миллисекунд в одном часе
+    * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+    */
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    /*
+    * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+    * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+    */
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    /*
+    * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+    * миллисекунд в одной секунде (1000)
+    */
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        
+    return { days, hours, mins, secs }
+    }
 
+    pad(value) {
+    return String(value).padStart(2, '0')
+    }
+};
 
+const timer = new CountdownTimer({
+        // selector: '#timer-1',
+        targetDate: new Date('Jul 28, 2021'),
+        onTick: updateInterface, 
+    });
 
-
-
+function updateInterface({ days, hours, mins, secs }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+ };
