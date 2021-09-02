599027a2573f7328e4bc92c3749cff1d0f45d904
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-09-02 19:01:05 +0300

    update css

--- src/index.js
+++ src/index.js
@@ -1,65 +1,67 @@
-import template from './template.hbs';
+import template from "./template.hbs";
 
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.mekeMarkup();
+    this.start();
+  }
 
-class CountdownTimer { 
-    constructor({selector, targetDate}) {
-        this.selector = selector;
-        this.targetDate = targetDate;
-        this.mekeMarkup();
-        this.start();
-        console.log(this.targetDate);
-        
-    }
+  mekeMarkup() {
+    const markup = template();
+    document.body.insertAdjacentHTML("afterbegin", markup);
+    document.querySelector(".timer").id = this.selector;
+  }
 
-    mekeMarkup() {
-        const markup = template();
-        document.body.insertAdjacentHTML('afterbegin', markup);
-        document.querySelector('.timer').id = this.selector;
-    }
-    
-    start() {
-         const refs = {
-            days: document.querySelector('span[data-value="days"]'),
-            hours: document.querySelector('span[data-value="hours"]'),
-            mins: document.querySelector('span[data-value="mins"]'),
-            secs: document.querySelector('span[data-value="secs"]')
-        }
+  start() {
+    const refs = {
+      days: document.querySelectorAll('span[data-value="days"]'),
+      hours: document.querySelectorAll('span[data-value="hours"]'),
+      mins: document.querySelectorAll('span[data-value="mins"]'),
+      secs: document.querySelectorAll('span[data-value="secs"]'),
+    };
 
-        setInterval(() => {
-            
-            const timeNow = Date.now();
-            const deltaTime = this.targetDate - timeNow;
-            const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
-            
-            refs.days.textContent = `${days}`;
-            refs.hours.textContent = `${hours}`;
-            refs.mins.textContent = `${mins}`;
-            refs.secs.textContent = `${secs}`
-            
-        }, 1000);
-    }
-   
-    
+    console.log(refs.days);
+    setInterval(() => {
+      const timeNow = Date.now();
+      const deltaTime = this.targetDate - timeNow;
+      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
 
-    getTimeComponents(time) {
-        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));   
-        return { days, hours, mins, secs }   
-        }
+      refs.days[0].textContent = `${days}`;
+      refs.days[1].textContent = `${days}`;
+      refs.hours[0].textContent = `${hours}`;
+      refs.hours[1].textContent = `${hours}`;
+      refs.mins[0].textContent = `${mins}`;
+      refs.mins[1].textContent = `${mins}`;
+      refs.secs[0].textContent = `${secs}`;
+      refs.secs[1].textContent = `${secs}`;
 
-    pad(value) {
-    return String(value).padStart(2, '0')
-        }
-};
+      refs.secs[1].classList.toggle("digit");
+    }, 1000);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
 
 const timer = new CountdownTimer({
-        selector: '#timer-1',
-        targetDate: new Date('Jul 28, 2021')
+  selector: "#timer-1",
+  targetDate: new Date("Jul 28, 2021"),
 });
 
 const timer2 = new CountdownTimer({
-        selector: '#timer-2',
-        targetDate: new Date('Jun 28, 2021')
-});      
+  selector: "#timer-2",
+  targetDate: new Date("Jun 28, 2021"),
+});
